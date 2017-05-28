fb49de86bd55fa4a2e5b4040b44edef112a54b0b
dkim6002 <Danielkim629@gmail.com>
2017-05-27 20:03:17 -0400

    user state and click events

--- game.js
+++ game.js
@@ -1,20 +1,24 @@
+const [p1, p2] = ['x', 'o'];
+let current_player = p1;
 
+let init = () => {
+	
+}
 
-
-let game = (function() {
-	return {
-		init: function() {
-
-		},
-		moves: function(id) {
-			console.log('click ' + id)
-		},
-		counter: function() {
-
-		},
-		restart: function() {
-
-		}
+let moves = (id) => {
+	console.log('click' + id);
+	let el = document.getElementById(id);
+	if (current_player === p1) {
+		el.innerHTML = p1;
+		el.removeAttribute('onClick');
+		current_player = p2
+	}else {
+		el.innerHTML = p2;
+		el.removeAttribute('onClick');
+		current_player = p1;
 	}
-}());
-game.init()
+}
+
+let restart = () => {
+
+}
